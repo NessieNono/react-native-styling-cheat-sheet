bb656eb5a5c61a4f53ee16707cc9bbda39b385c8
Ruby Bie <ruby@oa-andream-1.uacwind.uac.edu.au>
2019-04-11 16:59:57 +1000

    using touchable opacity

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
+import { dimensions, colors, padding, fonts } from "./styles/names";
 import { Container, Title } from "./styles/common";
 
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import ToggleView from "./components/ToggleView";
 
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.rosepink,
+    flex: 0.1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
 class Home extends React.Component {
   render() {
     return (
       <Container>
         <Title>Core Components</Title>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text> Touch Here </Text>
+        </TouchableOpacity>
         <Button
+          style={styles.button}
           title="Toggle View"
           onPress={() => this.props.navigation.navigate("View")}
         />
         <Button
+          style={styles.button}
           title="Toggle Flexbox"
           onPress={() => this.props.navigation.navigate("Flexbox")}
         />
